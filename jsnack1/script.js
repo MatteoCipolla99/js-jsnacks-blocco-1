cbfa1015a0b90049391bbb39662fbb2f0e687d36
Matteo Cipolla <mati_teo@live.it>
2024-12-03 11:39:10 +0100

    feat:add fisrt exercise

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+const num1 =prompt("Inserisci il primo numero");
+const num2 =prompt("Inserisci il secondo numero");
+
+if (num1 > num2){
+    alert("Il maggiore è num1");
+} else if (num2 > num1) {
+    alert("Il maggiore è num2");
+} else {
+    alert("I numeri sono uguali.");
+}
+
